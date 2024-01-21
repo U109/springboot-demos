5185e3bbacc8589b9fc828cbe0dddccb7230a9fb
zhangzhongzhen <1098598203@qq.com>
2024-01-21 22:06:52 +0800

    rabbitmq

---
+++ java-code/src/main/java/com/zzz/javacode/threads/ThreadLocalMain.java
@@ -0,0 +1,14 @@
+package com.zzz.javacode.threads;
+
+/**
+ * @author zhangzhongzhen wrote on 2024/1/21
+ * @version 1.0
+ * @description:
+ */
+public class ThreadLocalMain {
+
+
+    public static void main(String[] args) {
+        ThreadLocal<String> threadLocal = new ThreadLocal<>();
+    }
+}
